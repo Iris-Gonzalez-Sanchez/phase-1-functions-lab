c494c168350adf2b7caa8445dc958fdc94a7b68a
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-05-17 22:23:16 -0400

    commit tree b2995a68a3f5a6a3915d5f495f83fbeaf13bdece

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    // if (blocks > 42) {
+    //     return (blocks - 42);
+    // }else if (blocks < 42) {
+    //     return (42- blocks);
+    // }
+    return Math.abs(42 - blocks)
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    // if (start > destination) {
+    //     return (start - destination)*264;
+    // } else if (destination > start) {
+    //     return (destination - start)*264;
+    return Math.abs((start - destination)*264);
+}
+    
+ function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination)
+    if (distanceInFeet <= 400) {
+        return 0;
+    } else if (distanceInFeet >= 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400)*.02;
+    } else if (distanceInFeet >= 2000 && distanceInFeet <= 2500) {
+        return 25;
+    } else if (distanceInFeet >= 2500) {
+        return "cannot travel that far";
+    }   
+ }   
